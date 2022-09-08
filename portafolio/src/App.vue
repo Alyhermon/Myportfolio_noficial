0ae629dece92ae25d124b45f85d17942f9f17365
Alyhermon <110520524+Alyhermon@users.noreply.github.com>
2022-09-07 22:16:55 -1200

    Header

--- portafolio/src/App.vue
+++ portafolio/src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
+    <Home/>
+
   </div>
 </template>
 
 <script>
+import Home  from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  *{
+  margin: 0px;
+  padding: 0px;
 }
 </style>
---
+++ portafolio/src/components/Home.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="Container">
+        <div class="header">
+            <div class="menu">
+                <ul>                
+                <a href="#"><li v-for="tareas in tasks">
+                {{tareas.task}}</li></a>
+            </ul>
+
+            </div>
+
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                Hello: 'Hola Vue js', 
+                Usuario:{
+                    Nombre: '<h1>Alexander</h1>',
+                    Chica: 'Alina'
+                }, 
+                showname: true, 
+                tasks:[
+                    {task:'Inicio'},
+                    {task:'Sobre mi'},
+                    {task:'Proyectos'},
+                    {task: 'Habilidades'},
+                    {task: 'Contactos'}
+
+                ]
+            }
+        }
+
+    }
+
+</script>
+
+<style media="screen">
+    .header .menu{
+        background-color: #BA7B7C;
+        height: 90px;
+        width: 100%;
+    }
+
+    ul{
+        padding-top: 30px;
+        margin-left: 900px;
+        list-style: none;
+    }
+
+    .menu ul li {
+        color: white;
+        font-size: 20px;
+        display: block;
+        padding-left: 20px;
+        float: left;
+    }
+
+    ul li a {
+        text-align: none;
+    }
+
+    
+
+
+</style>
